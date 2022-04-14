93b1f00f9ae95e244325a123a9409e02546bbdb9
Your Name <you@example.com>
2022-04-14 10:44:12 +0200

    maj interfaces

--- src/application/LanceurProjet.java
+++ src/application/LanceurProjet.java
@@ -7,6 +7,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 
 
@@ -17,12 +18,13 @@ public class LanceurProjet extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("interfaces/InterfaceModifierStagiaire.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("interfaces/InterfaceAdministrateur.fxml"));
 			Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
 //			scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			primaryStage.setTitle("Annuaire Informatisé par FHF");
+			primaryStage.getIcons().add(new Image("C:\\Users\\farah\\Desktop\\FormationIsika\\ProjetsEclipse\\LogicielGestionStagiaires\\iconeTel.jpg"));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/application/controllers/InterfaceProposCtrl.java
@@ -0,0 +1,57 @@
+package application.controllers;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.ImageView;
+
+public class InterfaceProposCtrl {
+    
+    @FXML
+    private Hyperlink goToIsikaWebsite;
+
+    @FXML
+    private Hyperlink goToIsikaYoutube;
+
+    @FXML
+    private Hyperlink sendEmailForm;
+
+    @FXML
+    private Hyperlink whoDidThis;
+    
+    @FXML
+    private ImageView goToIsikaWebsiteImg;
+    
+    
+    
+    // METHODE POUR ALLER SUR LE SITE ISIKA//
+    @FXML
+    public void goToIsikaWebsite() throws IOException, URISyntaxException {
+	Desktop d = Desktop.getDesktop();
+	d.browse(new URI("https://projet-isika.com/"));
+    }
+    
+    // METHODE POUR ALLER SUR LA PAGE YOUTUBE ISIKA//
+    @FXML
+    public void goToIsikaYouTube() throws IOException, URISyntaxException {
+	Desktop d = Desktop.getDesktop();
+	d.browse(new URI("https://www.youtube.com/channel/UCyZGOAMaVNPITntX2HwbiSw/featured"));
+    }
+    
+    // METHODE QUI A FAIT CE PROJET//
+    public void propos() throws IOException {
+	Alert alert = new Alert(AlertType.INFORMATION);
+	alert.setTitle("Qui a fait ce projet?");
+	alert.setHeaderText("FHF ANNUAIRE INFORMATISÉ");
+	alert.setContentText("Projet 1 CDA17");
+	alert.showAndWait();
+    }
+
+}
+
